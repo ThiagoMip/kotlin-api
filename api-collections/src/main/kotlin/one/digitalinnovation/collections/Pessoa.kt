57d5898d3e50cb6ecfd8b56938e0fe8a7429f808
Thiago Prieto <thiagomirp@gmail.com>
2022-07-28 02:20:36 -0300

    updated to apply the private modifier to set function of cpf property

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -3,11 +3,20 @@ package one.digitalinnovation.collections
 class Pessoa {
     var nome:String = "Thiago"
     var cpf:String = "123.456.789-0"
+
+    private set
+
+    inner class  Endereco {
+        var rua:String = "Rua teste"
+
+    }
 }
 
 fun main(){
     val thiago = Pessoa()
 
     println(thiago.nome)
-    print(thiago.cpf)
+    println(thiago.cpf)
+
+    println(thiago.Endereco().rua)
 }
