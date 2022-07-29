d2bc87b1e1584a3f30b81de614cad3b6d8a317b4
Thiago Prieto <thiagomirp@gmail.com>
2022-07-29 00:50:57 -0300

    Updates to make, person and employee abstract class

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -1,17 +1,6 @@
 package one.digitalinnovation.collections
 
-class Pessoa {
-    var nome:String = "Thiago"
-    var cpf:String = "123.456.789-0"
-
-    private set
-
-    fun pessoaInfo() = "$nome + $cpf"
-}
-
-fun main(){
-    val thiago = Pessoa()
-
-    println(thiago.pessoaInfo())
-
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+        )
