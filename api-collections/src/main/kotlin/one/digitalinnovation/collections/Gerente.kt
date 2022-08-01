e74621a1bcdef1dad8199935e6da3f7990d63122
Thiago Prieto <thiagomirp@gmail.com>
2022-08-01 15:40:09 -0300

    created subclasses and analyst

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Gerente.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.collections
+
+class Gerente(
+    nome: String,
+    cpf: String,
+    salario: Double
+) : Funcionario(nome = nome, cpf = cpf, salario = salario) {
+    override fun calculoAuxilio(): Double = salario * 0.4
+
+}
