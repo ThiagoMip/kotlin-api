14146802b50425071399f42e400a62bc5a648349
Thiago Prieto <thiagomirp@gmail.com>
2022-08-01 16:15:16 -0300

    update function

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/teste/TesteFuncionarioGerente.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/teste/TesteFuncionarioGerente.kt
@@ -3,7 +3,9 @@ package one.digitalinnovation.collections.teste
 import one.digitalinnovation.collections.Gerente
 
 fun main(){
-    val claudia = Gerente(nome = "Claudia", cpf = "098.765.432-1", salario = (5000.0))
+    val claudia = Gerente(nome = "Claudia", cpf = "098.765.432-1", salario = (5000.0), senha = "senha123")
 
     ImprimeRelatorioFuncionario.imprime(claudia)
+
+    TesteAutenticacao().auntentica(claudia)
 }
